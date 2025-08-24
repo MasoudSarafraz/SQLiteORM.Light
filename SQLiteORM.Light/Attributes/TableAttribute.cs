1a268f91175dc69a25851add36726ba8463fc281
Masoud <Masoud@DESKTOP-HRNA1L2>
2025-08-24 18:22:47 +0330

    Change

---
+++ SQLiteORM.Light/Attributes/TableAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLiteORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        public string Name { get; }
+
+        public TableAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
